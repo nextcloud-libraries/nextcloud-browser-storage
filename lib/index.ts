7c9026d6cb5369252ba29e0f7fa3f9666273d610
Ferdinand Thiessen <opensource@fthiessen.de>
2024-04-21 16:07:18 +0200

    fix: Resolve linter issues
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- lib/index.ts
+++ lib/index.ts
@@ -1,28 +1,43 @@
 import StorageBuilder from './storagebuilder'
 import ScopedStorage from './scopedstorage'
 
+/**
+ * Get the storage builder for an app
+ * @param appId App ID to scope storage
+ */
 export function getBuilder(appId: string): StorageBuilder {
-    return new StorageBuilder(appId)
+	return new StorageBuilder(appId)
 }
 
-function clearStorage(storage: Storage, pred?: (string) => boolean): void {
-    Object.keys(storage)
-        .filter(k => pred ? pred(k) : true)
-        .map(storage.removeItem.bind(storage))
+/**
+ * Clear values from storage
+ * @param storage The storage to clear
+ * @param pred Callback to check if value should be cleared
+ */
+function clearStorage(storage: Storage, pred?: (value: string) => boolean): void {
+	Object.keys(storage)
+		.filter(k => pred ? pred(k) : true)
+		.map(storage.removeItem.bind(storage))
 }
 
+/**
+ * Clear all values from all storages
+ */
 export function clearAll(): void {
-    const storages = [
-        window.sessionStorage,
-        window.localStorage,
-    ]
-    storages.map(s => clearStorage(s))
+	const storages = [
+		window.sessionStorage,
+		window.localStorage,
+	]
+	storages.map(s => clearStorage(s))
 }
 
+/**
+ * Clear ony non persistent values
+ */
 export function clearNonPersistent(): void {
-    const storages = [
-        window.sessionStorage,
-        window.localStorage,
-    ]
-    storages.map(s => clearStorage(s, k => !k.startsWith(ScopedStorage.GLOBAL_SCOPE_PERSISTENT)))
+	const storages = [
+		window.sessionStorage,
+		window.localStorage,
+	]
+	storages.map(s => clearStorage(s, k => !k.startsWith(ScopedStorage.GLOBAL_SCOPE_PERSISTENT)))
 }
