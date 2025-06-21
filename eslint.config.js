8ac1c39f0d997403cd94457cba2a7568c553afea
Grigorii K. Shartsev <me@shgk.me>
2025-06-21 14:45:42 +0200

    chore(eslint): migrate to ESLint 9
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

---
+++ eslint.config.js
@@ -0,0 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+const { recommendedLibrary } = require('@nextcloud/eslint-config')
+
+module.exports = [...recommendedLibrary]
